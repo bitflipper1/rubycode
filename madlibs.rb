2d052a42ef859309345ef2f5a524ef5f70117e9d
bitflipper <mattmcg@bitfliptech.com>
2015-04-30 00:07:16 +0000

    madlibs

---
+++ madlibs.rb
@@ -0,0 +1,25 @@
+puts "Welcome to Mad Libs."
+
+
+puts "what is your name"
+name = gets 
+
+puts "What is your favorite food?"
+food = gets
+
+puts "How would you desciber yourself in one word"
+run = gets
+
+puts "What is your favoite exercise?"
+exercise = gets
+
+puts "What is your favoite car?"
+car = gets
+
+puts "Who is your favorite athlete"
+athlete = gets
+
+
+
+puts "My name is " + name.to_s "I went to the " verb.to_s adjective.to_s noun.to_s
+
